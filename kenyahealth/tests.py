64f695d344107a31a45e86eefb16b18a16cce5a1
emmanuel langat <emmanuelthedeveloper@gmail.com>
2019-03-12 21:39:12 +0300

    run the tests and ensured that they pass

--- kenyahealth/tests.py
+++ kenyahealth/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Doctor,Hospital,History,Patient
+from .models import Doctor,History,Patient
 from django.contrib.auth.models import User
 # Create your tests here.
 class DoctorTestClass(TestCase):
@@ -10,11 +10,11 @@ class DoctorTestClass(TestCase):
         self.manulangat = Doctor(user=self.manu,doctor_no=4689)
         self.manulangat.save()
     def test_instance(self):
-        assertTrue(isinstance(self.manulangat,Doctor))
+        self.assertTrue(isinstance(self.manulangat,Doctor))
     def test_save_doctor(self):
         self.manulangat.save_doctor()
         doc = Doctor.objects.all()
-        assertTrue(len(doc) > 0 )
+        self.assertTrue(len(doc) > 0 )
     def tearDown(self):
         User.objects.all().delete()
         Doctor.objects.all().delete()
@@ -28,11 +28,11 @@ class HistoryTestClass(TestCase):
         self.hist = History(text='helo there',by=self.manulangat)
         self.hist.save()
     def test_instance(self):
-        assertTrue(isinstance(self.hist,History))
+        self.assertTrue(isinstance(self.hist,History))
     def test_save_hist(self):
         self.hist.save_hist()
         hist = History.objects.all()
-        assertTrue(len(hist) > 0)
+        self.assertTrue(len(hist) > 0)
     def tearDown(self):
         User.objects.all().delete()
         Doctor.objects.all().delete()
@@ -49,11 +49,11 @@ class PatientTestClass(TestCase):
         self.pat = Patient(name='emmanuel',age=48,patient_no=6702,id_no=35903423,history=self.hist)
         self.pat.save()
     def test_instance(self):
-        assertTrue(isinstance(self.pat,Patient))
+        self.assertTrue(isinstance(self.pat,Patient))
     def test_save_pati(self):
         self.pat.save_pati()
         pati = Patient.objects.all()
-        assertTrue(len(pati) > 0)
+        self.assertTrue(len(pati) > 0)
     def tearDown(self):
         User.objects.all().delete()
         Doctor.objects.all().delete()
