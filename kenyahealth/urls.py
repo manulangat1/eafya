0e9d510e72a2203858ed27aea2e0a79fdb029407
emmanuel langat <emmanuelthedeveloper@gmail.com>
2019-03-12 21:18:31 +0300

    running the initial migrations.

--- kenyahealth/urls.py
+++ kenyahealth/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 #create your url patterns here
 urlpatterns =[
-   url(r'^$',views.index,name=index)
+   url(r'^$',views.index,name="index")
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
